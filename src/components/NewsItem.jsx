84709bb90804b76f8e73c505c3a66d6aed6c6918
taufiq <tuafiq8214829@gmail.com>
2025-12-30 22:12:26 +0700

    commit ke 9

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,8 +1,8 @@
 
 const NewsItem = ({title, description, src, url}) => {
     return (
-        <div className="card" style={{maxWidth:"345px"}}>
-  <img src={src} className="card-img-top" alt="..." />
+        <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
+  <img src={src} style={{}} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
